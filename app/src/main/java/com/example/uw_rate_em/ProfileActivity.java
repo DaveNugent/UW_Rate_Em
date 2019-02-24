447a5b1d25b673dc48b42b9bc8c23eeb4ba2fefb
Dave Nugent <dave.nuge22@gmail.com>
2019-02-24 02:06:57 -0600

    adding user courses to arraylist

--- app/src/main/java/com/example/uw_rate_em/ProfileActivity.java
+++ app/src/main/java/com/example/uw_rate_em/ProfileActivity.java
@@ -1,17 +1,30 @@
 package com.example.uw_rate_em;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private Button logout;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mCourseRef = mRootRef.child("Courses");
+    ArrayList<String> userCoursesArrayList = new ArrayList<>();
+    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser() ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,23 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity((new Intent(ProfileActivity.this, MainActivity.class)));
             }
         });
-        //TODO add course display based on uid
+        //TODO add course display from userCourseArrayList (I didn't run and test this code yet)
+
+        mCourseRef.child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot data: dataSnapshot.getChildren()) {
+                    // adding course to local array list if it is not already there
+                    if (!(data.getValue() == null) && !userCoursesArrayList.contains(data.getKey())) {
+                        userCoursesArrayList.add(data.getKey().toUpperCase());
+                    }
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
